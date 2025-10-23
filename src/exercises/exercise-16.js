6b227de18dc86683e06affbbac90baeed0ddfaab
Victor Parras <vipoo.info@gmail.com>
2025-10-23 19:33:55 +0200

    Exercise 15 return fixed & exercise 16 done

--- src/exercises/exercise-16.js
+++ src/exercises/exercise-16.js
@@ -9,6 +9,24 @@ Nota: asegurate que no hay titulos duplicados
 */
 export function exercise16(movies) {
   const actors = ["Leonardo DiCaprio", "Robert De Niro", "Tom Hanks"];
+  const bestMovies = [];
 
-  // Escribe tu solución aquí
+  actors.forEach(actor => {
+    const actorMovies = movies.filter(m => m.actors.includes(actor));
+
+    if (actorMovies.length > 0) {
+    
+      const bestMovie = actorMovies.reduce((max, movie) => {
+        if (movie.rating > max.rating) {
+          return movie;
+        } else {
+          return max;
+        }
+      });
+     
+      bestMovies.push(bestMovie.title);
+    }
+  });
+
+  return bestMovies;
 }
